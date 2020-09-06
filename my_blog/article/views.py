168cd4b7433288d1409b64ac1bb457b5c2322af5
BruceBond <jingyu.bruce.pang@gmail.com>
2020-09-06 15:07:23 +0800

    this is my_blog

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -20,3 +20,17 @@ def article_detail(request, id):
 
     context = {'article':article}
     return render(request, 'article/detail.html', context)
+def article_create(request):
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_vaild():
+            new_article = article_post_form.save(commit=False)
+            new_article.author = User.objects.get(id=1)
+            new_article.save()
+            return redirect("article:article_list")
+        else:
+            return HTTPresponse("表单内有误，请重新填写。")
+    else:
+        article_post_form = ArticlePostForm()
+        context = {'article_post_form': article_post_form}
+        return render(request,'article/create.html', context)
